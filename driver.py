24cab38c67960bdcb4bed42602ea73bd09c68a96
bradnowak <20384954+Bradnowak@users.noreply.github.com>
2020-03-18 14:15:26 -0500

    Update driver.py

--- driver.py
+++ driver.py
@@ -1,10 +1,6 @@
 import extractor
 import visualizer
 
-#sims = extractor5.Extractor("C:/Users/bradn/Downloads/Resume&Job_Description/data/")
-
-sims = extractor5_trigrams.Extractor("file path here")
-
-#sims = extractor5.Extractor("C:/Users/bradn/Documents/Presentation_Docs/")
+sims = extractor.Extractor("file path here")
 
 sims.analyze(n=10, topics=25)
